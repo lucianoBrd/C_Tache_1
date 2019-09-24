0eed8c7727c4f35bf90509f6aceb6696ae173558
Burdet <lucien.burdet@gmail.com>
2019-09-24 17:42:02 +0200

    comment .h

--- client.h
+++ client.h
@@ -7,10 +7,16 @@
  */
 int envoie_recois_message(int socketfd);
 
+/* envoi et reception de nom du client
+ */
 int envoie_nom_client(int socketfd);
 
+/* envoi de l'opération et reception du resultat
+ */
 int envoie_operateur_numero(int socketfd);
 
+/* envoi de couleurs et reception de confirmation
+ */
 int envoie_couleurs(int socketfd);
 
 #endif
--- serveur.h
+++ serveur.h
@@ -9,10 +9,16 @@
  */
 int recois_envoie_message(int socketfd);
 
+/* Renvoi le nom du client
+ */
 int renvoie_nom_client(int client_socket_fd, char *data);
 
+/* Renvoi le resultat du calcule ou -1 en cas d'erreur
+ */
 int recois_numero_calcule(int client_socket_fd, char *data);
 
+/* Renvoi la confirmation de sauvegarde des couleurs
+ */
 int recois_couleurs(int client_socket_fd, char *data);
 
 #endif
